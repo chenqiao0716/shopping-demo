4b50eed65df2ff664abcea847b720dd5406f1d14
joker_huiwtech <Huiwtech_joker2@git.cloud.tencent.com>
2018-05-29 15:46:07 +0800

    初始化代码

---
+++ src/shopping_miniPro/pages/index/index.js
@@ -0,0 +1,28 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '微信小程序测试',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
